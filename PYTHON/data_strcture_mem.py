91ad990e16a7ddc965bf857c196a81ca52f55098
AK <arulinmail@gmail.com>
2024-08-15 14:58:50 -0400

    array

---
+++ PYTHON/data_strcture_mem.py
@@ -0,0 +1,8 @@
+import numpy as np
+array1=np.array([0,1,2,3])
+array2=np.array([[[0,1],[0,2]],[[1,1],[1,2]],[[2,2],[2,0]]])
+array3=np.array([[[0,1],[0,2]],[[1,1],[1,2]]])
+print("1d-\n",array1)
+print("2d-\n",array2)
+
+print("2d-\n",array3)
