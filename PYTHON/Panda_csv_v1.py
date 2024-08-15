21c1ff04324191dec4363c3e9004fcae3173b034
AK <arulinmail@gmail.com>
2024-08-15 14:27:35 -0400

    commitv10

--- PYTHON/Panda_csv_v1.py
+++ PYTHON/Panda_csv_v1.py
@@ -1,7 +1,20 @@
 ##Learning to use panda for excel -commting first
 import pandas as pd
 from datetime import datetime
-
 # Load the Excel file
-excel_file = r'C:\Users\akubendran\Documents\NESSCAN_CTIS\T01.xlsx'.replace('\\', '/')
+excel_file = r'C:\Users\akubendran\AWS\T01.xlsx'.replace('\\', '/')
 df = pd.read_excel(excel_file)
+
+#Decode
+df_unique=df.drop_duplicates()
+df_duplicate=df[df.duplicated(keep=False)]
+
+#transform
+
+
+
+#report
+print("totalrows:",len(df))
+print("df_uniquetotalrows:",len(df_unique))
+print("df_df_duplicatetotalrows:",len(df_duplicate))
+print (df_duplicate)
