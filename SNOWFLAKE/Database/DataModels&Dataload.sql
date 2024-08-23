5e49c5a27874632edf365b0a4b922f42a70396ee
Arulingit <164373444+Arulingit@users.noreply.github.com>
2024-08-23 17:43:11 -0400

    Update DataModels&Dataload.sql

--- SNOWFLAKE/Database/DataModels&Dataload.sql
+++ SNOWFLAKE/Database/DataModels&Dataload.sql
@@ -145,7 +145,8 @@ select count(*) from NUCOR_PRODREP.REVENUE_REPORT_MANAGER.TRANSFORMEDREVENUE lim
 
 CREATE OR REPLACE TASK Load_DimDate
 WAREHOUSE = COMPUTE_WH
-SCHEDULE = 'USING CRON */2 * * * * UTC'
+SCHEDULE = 'USING CRON 0 */3 * * * UTC'
+--SCHEDULE = 'USING CRON /10 * * * * UTC'
 AS
 INSERT INTO DimDate (DateKey, Year, Quarter, Month, Day)
 SELECT DISTINCT 
@@ -156,6 +157,19 @@ SELECT DISTINCT
     DAY(RevenueDate) AS Day
 FROM NUCOR_PRODREP.REVENUE_REPORT_MANAGER.TRANSFORMEDREVENUE
 WHERE RevenueDate IS NOT NULL limit 10;
+
+CREATE OR REPLACE TASK Load_DimCustomer
+WAREHOUSE = COMPUTE_WH
+AFTER Load_DimDate
+AS
+INSERT INTO DimCustomer (CustomerID, CustomerName, CustomerSegment)
+SELECT DISTINCT 
+    CustomerID, 
+    -- Assuming CustomerName and CustomerSegment are columns in the source table
+    'Unknown' AS CustomerName,  -- Placeholder or default value
+    'Unknown' AS CustomerSegment  -- Placeholder or default value
+FROM NUCOR_PRODREP.REVENUE_REPORT_MANAGER.TransformedCustomerInsights;
+
 
 CREATE OR REPLACE TASK Load_DimCustomer
 WAREHOUSE = COMPUTE_WH
