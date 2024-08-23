100b2234262f16c3503ba2426f375bed57e28b17
Arulingit <164373444+Arulingit@users.noreply.github.com>
2024-08-23 17:40:40 -0400

    Update DataModels&Dataload.sql

--- SNOWFLAKE/Database/DataModels&Dataload.sql
+++ SNOWFLAKE/Database/DataModels&Dataload.sql
@@ -56,7 +56,12 @@ CREATE TABLE FactCustomerInsights (
 ALTER TABLE NUCOR_PRODREP.REVENUE_REPORT_MANAGER.TRANSFORMEDREVENUE
 ALTER COLUMN ProfitMargin SET DATA TYPE NUMBER(6,2);
 
-
+use database NUCOR_PRODREP;
+use schema Revenue_report_Manager;
+ALTER TABLE NUCOR_PRODREP.REVENUE_REPORT_MANAGER.TRANSFORMEDREVENUE
+ALTER COLUMN ProfitMargin SET DATA TYPE NUMBER(6,2);
+-- Create a stored procedure to dynamically load data
+-- Sample SQL to create 500 records
 CREATE OR REPLACE PROCEDURE load_transformed_data_Revenue()
   RETURNS STRING
   LANGUAGE JAVASCRIPT
@@ -90,8 +95,8 @@ $$;
 -- Call the procedure to load data
 CALL load_transformed_data_Revenue();
 
-
-
+use database NUCOR_PRODREP;
+use schema Revenue_report_Manager;
 
 -- Create a stored procedure to dynamically load data
 CREATE OR REPLACE PROCEDURE load_transformed_data_CUSTOMERs()
@@ -132,13 +137,15 @@ truncate table  NUCOR_PRODREP.REVENUE_REPORT_MANAGER.TRANSFORMEDREVENUE
 -- Call the procedure to load data
 CALL load_transformed_data_CUSTOMERs();
 
+truncate table DimDate;
 
-
+select count(*) from NUCOR_PRODREP.REVENUE_REPORT_MANAGER.TRANSFORMEDCUSTOMERINSIGHTS limit 10;
+select count(*) from NUCOR_PRODREP.REVENUE_REPORT_MANAGER.TRANSFORMEDREVENUE limit 10;
 
 
 CREATE OR REPLACE TASK Load_DimDate
 WAREHOUSE = COMPUTE_WH
-SCHEDULE = 'USING CRON */10 * * * * UTC'
+SCHEDULE = 'USING CRON */2 * * * * UTC'
 AS
 INSERT INTO DimDate (DateKey, Year, Quarter, Month, Day)
 SELECT DISTINCT 
@@ -148,20 +155,23 @@ SELECT DISTINCT
     MONTH(RevenueDate) AS Month, 
     DAY(RevenueDate) AS Day
 FROM NUCOR_PRODREP.REVENUE_REPORT_MANAGER.TRANSFORMEDREVENUE
-WHERE RevenueDate IS NOT NULL;
+WHERE RevenueDate IS NOT NULL limit 10;
 
-CREATE TASK Load_DimCustomer
+CREATE OR REPLACE TASK Load_DimCustomer
 WAREHOUSE = COMPUTE_WH
 AFTER Load_DimDate
 AS
-INSERT INTO DimCustomer
-SELECT DISTINCT CustomerID, CustomerName, CustomerSegment
+INSERT INTO DimCustomer (CustomerID, CustomerName, CustomerSegment)
+SELECT DISTINCT 
+    CustomerID, 
+    -- Assuming CustomerName and CustomerSegment are columns in the source table
+    'Unknown' AS CustomerName,  -- Placeholder or default value
+    'Unknown' AS CustomerSegment  -- Placeholder or default value
 FROM NUCOR_PRODREP.REVENUE_REPORT_MANAGER.TransformedCustomerInsights;
 
 
----FACT tables loaded by TASK 
 
-CREATE TASK Load_FactCustomerInsights
+CREATE OR REPLACE TASK  Load_FactCustomerInsights
 WAREHOUSE = COMPUTE_WH
 AFTER Load_DimCustomer
 AS
@@ -172,9 +182,11 @@ SELECT
     tci.SatisfactionScore,
     tci.TotalPurchases
 FROM TransformedCustomerInsights tci
-JOIN DimDate dd ON tci.LastPurchaseDate = dd.DateKey;
+JOIN DimDate dd ON tci.LastPurchaseDate = dd.DateKey  limit 10;
 
-CREATE TASK Load_FactRevenue
+
+
+CREATE OR REPLACE TASK Load_FactRevenue
 WAREHOUSE = COMPUTE_WH
 AFTER Load_DimDate
 AS
@@ -185,4 +197,28 @@ SELECT
     tr.CostOfGoodsSold, 
     tr.ProfitMargin
 FROM TransformedRevenue tr
-JOIN DimDate dd ON tr.RevenueDate = dd.DateKey;
+JOIN DimDate dd ON tr.RevenueDate = dd.DateKey limit 10;
+
+
+ALTER TASK Load_DimDate RESUME;
+ALTER TASK Load_DimCustomer RESUME;
+ALTER TASK Load_FactCustomerInsights RESUME;
+ALTER TASK Load_FactRevenue RESUME;
+
+ALTER TASK Load_DimDate SUSPEND;
+ALTER TASK Load_DimCustomer SUSPEND;
+ALTER TASK Load_FactCustomerInsights SUSPEND;
+ALTER TASK Load_FactRevenue SUSPEND;
+
+select * from DimDate;
+select * from FactRevenue;
+select * from DimCustomer;
+select * from FactCustomerInsights;
+
+select * from NUCOR_PRODREP.REVENUE_REPORT_MANAGER.TRANSFORMEDREVENUE limit 2;
+select * from NUCOR_PRODREP.REVENUE_REPORT_MANAGER.TRANSFORMEDCUSTOMERINSIGHTS limit 2;
+
+truncate table DimDate;
+truncate table DimCustomer;
+truncate table FactRevenue;
+truncate tableFactCustomerInsights;
