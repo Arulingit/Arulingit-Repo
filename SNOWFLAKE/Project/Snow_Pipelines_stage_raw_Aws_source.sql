56ac7089b457f52fd678ca470a8c71d4a21fc2cf
Arulingit <164373444+Arulingit@users.noreply.github.com>
2024-08-24 18:27:55 -0400

    Update Snow_Pipelines_stage_raw_Aws_source.sql

--- SNOWFLAKE/Project/Snow_Pipelines_stage_raw_Aws_source.sql
+++ SNOWFLAKE/Project/Snow_Pipelines_stage_raw_Aws_source.sql
@@ -12,25 +12,13 @@ storage_aws_role_arn='arn:aws:iam::876809972236:role/Welcomeguest'
 storage_allowed_locations=('s3://01loaddatatosnowflake/');
 
 desc integration aws_snf_data
-create or replace file format csv_load_format
-type='CSV'
-compression='AUTO'
-field_delimiter=','
-record_delimiter='\n'
-skip_header=1
-field_optionally_enclosed_by='\042'
-trim_space=FALSE
-error_on_column_count_mismatch=TRUE
-escape='NONE'
-escape_unenclosed_field='\134'
-date_format='AUTO'
-timestamp_format='AUTO';
+
 
 create or replace stage Aws_Snowflake_csv
 storage_integration=aws_snf_data                                                                
 url='s3://01loaddatatosnowflake/csv'
 file_format=csv_load_format;
-list @stage_csv_file
+list @Aws_Snowflake_csv
 
 
 
@@ -42,19 +30,39 @@ CREATE OR REPLACE TABLE RawRevenueData_csv (
     CostOfGoodsSold DECIMAL(10,2),
     ProfitMargin DECIMAL(10,2)
 );
+CREATE OR REPLACE FILE FORMAT csv_load_format
+  TYPE = 'CSV'
+  FIELD_OPTIONALLY_ENCLOSED_BY = '"'
+  FIELD_DELIMITER = ','
+  RECORD_DELIMITER = '\n'
+  SKIP_HEADER = 1
+  NULL_IF = ('')
+  EMPTY_FIELD_AS_NULL = TRUE
+  DATE_FORMAT = 'AUTO'
+  TIMESTAMP_FORMAT = 'AUTO';
 
 
+list @Aws_Snowflake_csv;
 
 
 select * from RawRevenueData_csv
 truncate table RawRevenueData_csv;
 
-CREATE OR REPLACE PIPE SNOWPIPELINE_2_AWS_S3 AUTO_INGEST=TRUE 
- AS
- COPY INTO RawRevenueData_csv  FROM @Aws_Snowflake_csv FILE_FORMAT = (TYPE = 'CSV') ON_ERROR=continue;;
+CREATE OR REPLACE PIPE SNOWPIPELINE_2_AWS_S3 
+AUTO_INGEST = TRUE 
+AS
+COPY INTO RawRevenueData_csv 
+FROM @Aws_Snowflake_csv
+FILE_FORMAT = (TYPE = 'CSV') 
+ON_ERROR = 'CONTINUE';
+SELECT * FROM TABLE(information_schema.query_history())
+WHERE query_text ILIKE '%COPY INTO%';
 
+show pipes
 
-           
+    ALTER PIPE SNOWPIPELINE_2_AWS_S3 SET PIPE_EXECUTION_PAUSED = TRUE;
+ALTER PIPE SNOWPIPELINE_2_AWS_S3 SET PIPE_EXECUTION_PAUSED = FALSE;
+ALTER PIPE SNOWPIPELINE_2_AWS_S3  REFRESH;       
 
 --OFFLOAD
 
