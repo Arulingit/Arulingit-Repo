011d1072bfc1c41b78a63049b80950cde87e9c8d
Arulingit <164373444+Arulingit@users.noreply.github.com>
2024-08-24 17:46:44 -0400

    Create Raw_stage_tables_scripts.sql

---
+++ SNOWFLAKE/Project/Raw_stage_tables_scripts.sql
@@ -0,0 +1,33 @@
+
+CREATE OR REPLACE TABLE RawRevenueData (
+    RevenueID STRING,
+    RevenueDate DATE,
+    TotalRevenue DECIMAL(10,2),
+    CostOfGoodsSold DECIMAL(10,2),
+    ProfitMargin DECIMAL(10,2)
+);
+
+CREATE OR REPLACE TABLE RawCustomerData (
+    CustomerID STRING,
+    SatisfactionScore INT,
+    Feedback STRING,
+    TotalPurchases INT,
+    LastPurchaseDate DATE
+);
+
+
+CREATE OR REPLACE TABLE RawRevenueData_csv (
+    RevenueID STRING,
+    RevenueDate DATE,
+    TotalRevenue DECIMAL(10,2),
+    CostOfGoodsSold DECIMAL(10,2),
+    ProfitMargin DECIMAL(10,2)
+);
+
+CREATE OR REPLACE TABLE RawCustomerData_json (
+    CustomerID STRING,
+    SatisfactionScore INT,
+    Feedback STRING,
+    TotalPurchases INT,
+    LastPurchaseDate DATE
+);
